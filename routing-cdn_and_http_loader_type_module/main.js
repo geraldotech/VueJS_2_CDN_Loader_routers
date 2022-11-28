780e70d55d339e98f488327b1c7b59a331c37900
geraldo.tech <geraldo.filho92@hotmail.com>
2022-11-28 14:08:00 -0300

    :zap: seveth commit

--- routing-cdn_and_http_loader_type_module/main.js
+++ routing-cdn_and_http_loader_type_module/main.js
@@ -1,22 +1,21 @@
 import Home from "./pages/home.vue.js";
 import About from "./pages/about.vue.js";
-import Assets from "./pages/assets.vue.js";
-import Contact from "./pages/contact.vue.js";
+//import Assets from "./pages/assets.vue.js";
 
 const app2 = new Vue({
   el: "#app2",
   data: {},
   components: {
     foo: httpVueLoader("./pages/footer.vue"),
+    sobre: httpVueLoader("./pages/sobre.vue"),
   },
 });
 
 var routes = [
   { path: "/", component: Home },
   { path: "/about", component: About },
-  { path: "/contact", component: Contact },
-  { path: "/assets", component: Assets },
-  //{ path: "/assets/files", component: "/assets/files/" },
+  //{ path: "/assets", component: Assets },
+  { path: "/sobre", component: httpVueLoader("./pages/sobre.vue") },
 ];
 
 var router = new VueRouter({
---
+++ routing-cdn_and_http_loader_type_module/pages/sobre.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h4>about page loaded from router</h4>
+  </div>
+</template>
+
+<style scoped>
+footer {
+  background: rgb(63, 39, 31);
+  height: 50px;
+  text-align: center;
+}
+</style>>
