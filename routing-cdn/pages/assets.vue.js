786a298a2ed84dffc6a57984c656690fa6dd1c09
geraldo.tech <geraldo.filho92@hotmail.com>
2022-11-28 13:26:47 -0300

    four commit

--- routing-cdn/pages/assets.vue.js
+++
@@ -1,3 +0,0 @@
-var Assets = {
-  template: `../img`,
-};
--- routing-cdn/pages/home.vue.js
+++
@@ -1,5 +0,0 @@
-var Home = {
-  template: `<div><h1>Home</h1><p>This is home page</p>
-	<a href="https://shouts.dev/articles/vue-js-routing-from-scratch-using-cdn-without-cli">REF</a>
-	</div>`,
-};
